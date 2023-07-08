1eece2a32561bdd6f8fab403ddacce69cdce30d5
faii <1320407956@qq.com>
2023-07-08 13:03:47 +0800

    merge0.1

--- kernel_module/.AuditModule.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/faii/Desktop/fileaudit/kernel_module/AuditModule.mod := printf '%s\n'   sdthook.o syscalltable.o netlinkp.o | awk '!x[$$0]++ { print("/home/faii/Desktop/fileaudit/kernel_module/"$$0) }' > /home/faii/Desktop/fileaudit/kernel_module/AuditModule.mod
